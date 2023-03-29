35d7b0a7361ca9bc38873c03b4bd1b5911ac647b
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-03-29 12:47:02 -0700

    Copied code over from prior project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,43 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from './components/Navbar'
 import ListStudents from './components/ListStudents'
+import WeatherForm from "./components/weatherForm";
+import WeatherCard from "./components/weatherCard";
 
 
 function App() {
+
+
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState(null);
+
+  const loadCity = () => {
+    fetch(`http://localhost:8080/weather?cityName=${city}`)
+      .then((response) => response.json())
+      .then((result) => {
+
+        // pass data from child to component
+        setResult(result);
+        console.log(city);
+        console.log(result);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loadCity();
+  };
 
   return (
     <div className="App">
       <MyNavBar />
       <ListStudents />
-
+  <WeatherForm setCity={setCity} handleSubmit={handleSubmit} />
+      {!result ? (
+        <p>Please click the botton to see Data</p>
+      ) : (
+        <WeatherCard data={result} />
+      )}
     </div>
   )
 }
---
+++ client/src/components/weatherCard.js
@@ -0,0 +1,52 @@
+const WeatherCard = ({ data }) => {
+    return (
+      <div className="weather-card">
+        <div className="result">
+          <p>
+            City:{" "}
+            <span className="data">
+              {data.name}, {data.sys.country}
+            </span>
+          </p>
+          <p>
+            Description:{" "}
+            <span className="data">{data.weather[0].description}</span>
+          </p>
+          <img
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`}
+            alt={"Icon from Open Weather Api"}
+          />
+          <p>
+            Temperature:{" "}
+            <span className="data">
+              {data.main.temp} <sup>o</sup>F
+            </span>
+          </p>
+          <p>
+            Feels Like:{" "}
+            <span className="data">
+              {data.main.feels_like} <sup>o</sup>F
+            </span>
+          </p>
+  
+          <p>
+            Pressure:{" "}
+            <span className="data">
+              {data.main.pressure} hPa
+            </span>
+          </p>
+  
+          <p>
+            Humidity Levels:{" "}
+            <span className="data">
+              {data.main.humidity} %
+            </span>
+          </p>
+  
+        </div>
+      </div>
+    );
+  };
+  
+  export default WeatherCard;
+  
