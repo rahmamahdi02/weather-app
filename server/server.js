8a9aee5b118d95437ef343ebc9d86c9fbe058ec8
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-03-29 12:34:59 -0700

    I havea added in initial files for weather app set up

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
 const db = require('./db/db-connection.js');
+const dataWeather = require("./data")
 
 
 const app = express();
@@ -83,6 +84,29 @@ app.put('/api/students/:studentId', async (req, res) =>{
       return res.status(400).json({e})
     }
   })
+
+
+  const API_KEY = "586b5c16d3a880172379a042ac42cf5f"
+
+  // creates an endpoint for the route /api/weather
+  app.get("/weather", (req, res) => {
+    const city = req.query.cityName;
+    console.log(req.query);
+    const apiKey = process.env.API_KEY;
+    const params = new URLSearchParams({
+      q: city,
+      appid: apiKey,
+      units: "imperial",
+    });
+    const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+    console.log(url);
+  
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => res.send(data))
+      .catch((err) => console.log(err));
+  });
+  
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
